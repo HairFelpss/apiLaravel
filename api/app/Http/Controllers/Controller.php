48b3440b3510f06377cc24c7c6d4b43443e6a901
Felipe Alves <fajreyy.bh@gmail.com>
2019-05-30 18:36:25 -0300

    commit

---
+++ api/app/Http/Controllers/Controller.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Route;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+    public function listPage (){
+        return view('index');
+    }
+
+    public function handleSearch(Request $request){     
+        $input = $request->input('input');
+
+        $apiQuote = 'https://api.iextrading.com/1.0/stock/' . $input . '/quote';
+        $apiLogo = 'https://api.iextrading.com/1.0/stock/' . $input . '/logo';
+        $apiNews = 'https://api.iextrading.com/1.0/stock/' . $input . '/news/last/1';
+
+        $url = array($apiQuote, $apiLogo, $apiNews);
+
+        $client = array();
+        $response = array(); 
+        $json = array();
+        $object = array();
+
+        for($i = 0; $i < count($url); $i++){
+            array_push($client, new Client(['base_uri' => $url[$i]]));
+            array_push($response, $client[$i]->request('GET'));
+            array_push($json, $response[$i]->getBody());
+            array_push($object, json_decode($json[$i]));
+            
+        }
+        $latestPrice = $object[0]->latestPrice ;
+        $name = $object[0]->companyName;
+        $logo = $object[1]->url;
+        $headline = $object[2][0]->headline;
+        $summary = $object[2][0]->summary;
+
+        return view('index', compact('name', 'latestPrice', 'logo', 'headline', 'summary')); 
+
+
+        /*if ($price && $name) {
+            $sql = 'INSERT INTO latestPrice(Enterprise, Price)  
+        VALUES(' + $name + ', ' + $price + ')';
+
+
+            connection.query(sql);
+
+            connection.end();
+            **/
+    }
+
+}
---
+++ api/resources/views/index.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/index.css') }}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+</head>
+
+<body class='page'>
+
+    <section class='container'>
+        <section>
+            <form action="/" method='post'>
+            @csrf
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" type="search" id="input" name="input" placeholder="Qual empresa deseja pesquisar">
+                        <div class="input-group-append">
+                          <button class="btn btn-outline-secondary" type='submit' id="button-addon2"> <i class="fa fa-search searchIcon"></i> </button>
+                        </div>
+                </div>
+            </form>
+        </section>
+
+        @if(isset($name))
+        <article>
+ 
+            <div class="card mb-3 cardContainer" style="max-width: 540px;">
+                <div class="row no-gutters">
+                        <div class="col-md-4 imgDIv">
+                            <img src="{{$logo}}" class="card-img logoImg" alt="...">
+                        </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h3 class="card-title">{{$name}}</h3>
+                            <h6 class="card-subtitle mb-2 text-muted">Selling price ${{$latestPrice}}</h6>
+                            <p class="card-text">{{$headline}}</p>
+                            <p class="card-text"><small class="text-muted">{{$summary}}</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </article>
+        @endif
+    </section>
+
+
+
+</body>
+
+</html>
